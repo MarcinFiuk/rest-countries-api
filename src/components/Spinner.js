8a034d44dd0ef0f7ec25d30d0e9897f829985fa0
Marcin_Fi <fiukmarcinn@gmail.com>
2022-05-23 11:09:55 +0200

    fix: border and border-color were in wrong order

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -13,20 +13,20 @@ const AAA = keyframes`
 
 const SpinnerStyled = styled.div`
     display: inline-block;
+    width: 6.25rem;
     height: 6.25rem;
     margin-inline: auto;
-    width: 6.25rem;
 
     &:after {
         animation: ${AAA} 1.2s linear infinite;
-        border-color: var(--textColor) transparent;
+        border: 0.5rem solid var(--textColor);
+        border-color: var(--textColor) transparent; //NOTE:border-color overwrite color assigned in border shortcut
         border-radius: 50%;
-        border: 0.5rem solid red;
         content: ' ';
         display: block;
         height: 5.625rem;
         margin: 0.5rem;
-        transition: border-color 0.3s ease-in;
+        transition: border 0.3s ease-in;
         width: 5.625rem;
     }
 `;
